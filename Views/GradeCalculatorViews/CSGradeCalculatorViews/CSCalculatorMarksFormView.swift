01ed316dfc24b2f456db01a1cdaaeecf05b38869
Steve Boyle <stephen_boyle48@outlook.com>
2025-07-02 13:24:13 +0100

    Lots of changed made. Just need to add accessibility features.

---
+++ Views/GradeCalculatorViews/CSGradeCalculatorViews/CSCalculatorMarksFormView.swift
@@ -0,0 +1,18 @@
+//
+//  CSCalculatorMarksFormView.swift
+//  GCSEProgressAnalyser
+//
+//  Created by Stephen Boyle on 02/07/2025.
+//
+
+import SwiftUI
+
+struct CSCalculatorMarksFormView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CSCalculatorMarksFormView()
+}
---
+++ Views/GradeCalculatorViews/CSGradeCalculatorViews/CSTotalMarksAndGradeView.swift
@@ -0,0 +1,18 @@
+//
+//  CSTotalMarksAndGradeView.swift
+//  GCSEProgressAnalyser
+//
+//  Created by Stephen Boyle on 24/06/2025.
+//
+
+import SwiftUI
+
+struct CSTotalMarksAndGradeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CSTotalMarksAndGradeView()
+}
